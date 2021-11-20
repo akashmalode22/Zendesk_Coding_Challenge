6f150cfa5827aaebfbc9a9907edb807e352115ce
Akash Malode <akashmalode22@gmail.com>
2021-11-20 16:20:03 -0500

    Connect to Zendesk API
    Attempt 1: Connect to Zendesk API with email and password. This works, but password is visible
    Attempt 2: Connect to Zendesk API with email and API token. Provides a layer of security since password is not visible

---
+++ ticketviewer.py
@@ -0,0 +1,20 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+import errormacros
+
+
+url = "https://zccakashmalode.zendesk.com/api/v2/tickets.json"
+
+user = "amalode@purdue.edu/token"
+pwd = "KCghGkIuanNLONrTjn6UuoCNr79VhqUR7koXjrG1"
+
+response = requests.get(url, auth=(user, pwd))
+
+if response.status_code != errormacros.GET_SUCCESS_CODE:
+    print("Status Code:", response.status_code, "Unable to execute GET request.")
+    exit()
+
+data = response.json()
+
+print(data["tickets"][0]["subject"])
