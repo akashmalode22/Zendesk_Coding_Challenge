9d4b60047883a44febd09e149c3100718afc74cb
Akash Malode <akashmalode22@gmail.com>
2021-11-24 23:58:42 -0500

    Unittest: Test all utils.py functions
    This commit:
      - Writes unit tests for testable utils.py functions

---
+++ test_utils.py
@@ -0,0 +1,71 @@
+import unittest
+from unittest.mock import patch
+import modes
+import retrievetickets
+import utils
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.mode = modes.Modes()
+        self.retriever = retrievetickets.RetrieveTickets()
+
+    def tearDown(self):
+        del self.mode
+
+    def test_initializeClassObjects(self):
+        [mode, retriever] = utils.initializeClassObjects()
+        self.assertEqual(type(mode), type(self.mode))
+        self.assertEqual(type(retriever), type(self.retriever))
+
+    def test_populateListWithIDs(self):
+        self.assertEqual(utils.populateListWithIDs(1, 5), ["1", "2", "3", "4", "5"])
+
+        with self.assertRaises(IndexError):
+            utils.populateListWithIDs(5, 2)
+            utils.populateListWithIDs(0, 2)
+            utils.populateListWithIDs(-1, -1)
+
+        with self.assertRaises(TypeError):
+            utils.populateListWithIDs(5, "2")
+            utils.populateListWithIDs("0", 2)
+            utils.populateListWithIDs(None, 10)
+
+    def test_calculateNextPageBounds(self):
+        self.assertEqual(utils.calculateNextPageBounds(25, 101), [26, 50])
+        self.assertEqual(utils.calculateNextPageBounds(50, 101), [51, 75])
+        self.assertEqual(utils.calculateNextPageBounds(100, 101), [101, 101])
+
+        with self.assertRaises(TypeError):
+            utils.calculateNextPageBounds(5, "2")
+            utils.calculateNextPageBounds("0", 2)
+            utils.calculateNextPageBounds(None, 10)
+
+    def test_calculatePreviousPageBounds(self):
+        self.assertEqual(utils.calculatePreviousPageBounds(26, 50), [1, 25])
+        self.assertEqual(utils.calculatePreviousPageBounds(101, 101), [76, 100])
+
+        with self.assertRaises(IndexError):
+            utils.calculatePreviousPageBounds(1, 25)
+
+        with self.assertRaises(TypeError):
+            utils.calculateNextPageBounds(5, "2")
+            utils.calculateNextPageBounds("0", 2)
+            utils.calculateNextPageBounds(None, 10)
+
+    def test_calculateTotalPages(self):
+
+        self.assertEqual(utils.calculateTotalPages(101), 5)
+        self.assertEqual(utils.calculateTotalPages(100), 4)
+        self.assertEqual(utils.calculateTotalPages(25), 1)
+        self.assertEqual(utils.calculateTotalPages(10), 1)
+        self.assertEqual(utils.calculateTotalPages(0), 0)
+
+        with self.assertRaises(TypeError):
+            utils.calculateTotalPages("2")
+            utils.calculateTotalPages(None)
+            utils.calculateTotalPages(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- utils.py
+++ utils.py
@@ -38,6 +38,11 @@ def getSubdomainFromFile(filename):
 
 def populateListWithIDs(start_id, end_id):
 
+    if start_id < 0 or end_id < 0 or end_id < start_id:
+        raise IndexError("invalid start_id or end_id.")
+    if not isinstance(start_id, int) or not isinstance(end_id, int):
+        raise TypeError("start_id and end_id must be integers.")
+
     # Store required ticket IDs in an integer list
     ids = []
     for i in range(start_id, end_id + 1):
@@ -50,6 +55,10 @@ def populateListWithIDs(start_id, end_id):
 
 
 def calculateNextPageBounds(last_ticket_shown, number_of_tickets):
+
+    if not isinstance(last_ticket_shown, int) or not isinstance(number_of_tickets, int):
+        raise TypeError("last_ticket_shown and number_of_tickets must be integers.")
+
     start_id = last_ticket_shown + 1
     end_id = min(last_ticket_shown + modes.TICKETS_PER_PAGE_LIMIT, number_of_tickets)
 
@@ -57,6 +66,13 @@ def calculateNextPageBounds(last_ticket_shown, number_of_tickets):
 
 
 def calculatePreviousPageBounds(start_id, end_id):
+
+    if start_id < modes.TICKETS_PER_PAGE_LIMIT or end_id < start_id:
+        raise IndexError("start_id is not in range.")
+
+    if not isinstance(start_id, int) or not isinstance(end_id, int):
+        raise TypeError("start_id and end_id must be integers.")
+
     start_id = start_id - modes.TICKETS_PER_PAGE_LIMIT
 
     if end_id % modes.TICKETS_PER_PAGE_LIMIT != 0:
@@ -69,6 +85,10 @@ def calculatePreviousPageBounds(start_id, end_id):
 
 
 def calculateTotalPages(number_of_tickets):
+
+    if not isinstance(number_of_tickets, int):
+        raise TypeError("number_or_tickets should be an integer")
+
     total_pages = 0
 
     total_pages += number_of_tickets / modes.TICKETS_PER_PAGE_LIMIT
