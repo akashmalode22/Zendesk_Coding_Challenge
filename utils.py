3b1a91c600480dd8dfac4e59ff16f1ef8c5fd86a
Akash Malode <akashmalode22@gmail.com>
2021-11-25 15:07:06 -0500

    Documentation: Docstring comments for utils.py
    This commit:
      - Adds docstring comments for all functions for utils.py
      - Follows the Google Format for docstrings

--- utils.py
+++ utils.py
@@ -3,6 +3,17 @@ import retrievetickets
 
 
 def initializeClassObjects():
+    """Instantiate Modes and RetrieveTickets class objects.
+
+    Objects for Modes and RetrieveTickets classes are instantiated and returned
+
+    Args:
+        no value
+
+    Returns:
+        mode: Object of Modes class
+        retriever: Object of RetrieveTickets class
+    """
 
     mode = modes.Modes()
     retriever = retrievetickets.RetrieveTickets()
@@ -11,6 +22,24 @@ def initializeClassObjects():
 
 
 def getDataFromFile(filename):
+    """Reads data from filename and converts into dictionary
+
+    Opens "filename" file. Reads contents line by line and separates
+    into key:value pairs based on ':' symbol. Returns credentials
+    structured as
+    {
+        subdomain:<subdomain>,
+        user:<user>,
+        token:<token>
+    }
+
+    Args:
+        filename (string): File to extract authorization credentials from
+
+    Returns:
+        credentials (dictionary): key-value pairs of credentials
+    """
+
     credentials = []
     try:
         file = open(filename)
@@ -30,6 +59,18 @@ def getDataFromFile(filename):
 
 
 def getCredentialsFromFile(filename):
+    """Extracts user and token from previously read file
+
+    Extracts the "user" and "token" fields from dictionary
+    which was read from filename. Returns the two fields
+
+    Args:
+        filename (string): File to extract authorization credentials from
+
+    Returns:
+        user (string): user extracted from filename
+        token (string): token extracted from filename
+    """
 
     credentials = getDataFromFile(filename)
 
@@ -37,12 +78,38 @@ def getCredentialsFromFile(filename):
 
 
 def getSubdomainFromFile(filename):
+    """Extracts subdomain from previously read file
+
+    Extracts the "subdomain" field from dictionary
+    which was read from filename. Returns the subdomain.
+
+    Args:
+        filename (string): File to extract authorization credentials from
+
+    Returns:
+        subdomain (string): subdomain extracted from filename
+    """
     credentials = getDataFromFile(filename)
 
     return credentials["subdomain"]
 
 
 def populateListWithIDs(start_id, end_id):
+    """Populates a list with numbers in range of start_id
+    to end_id
+
+    Populates a list with IDs from a start_id to end_id. Converts all
+    values to strings. This list is used to retrieve certain tickets with
+    IDs from the API.
+
+    Args:
+        start_id (integer): Starting index for range to populate list with
+        end_id (integer): Ending index for range to populate list with
+                          (inclusive)
+
+    Returns:
+        ids (list of strings): Requested ID numbers converted to strings
+    """
 
     if start_id < 0 or end_id < 0 or end_id < start_id:
         raise IndexError("invalid start_id or end_id.")
@@ -61,6 +128,28 @@ def populateListWithIDs(start_id, end_id):
 
 
 def calculateNextPageBounds(last_ticket_shown, number_of_tickets):
+    """Calculates the range of tickets required to display the next page
+    of tickets
+
+    Calculating the range of tickets for the next page depends on the
+    last ticket shown and the total number of tickets. For the next page,
+    the start index is just incremented, but the end index can either be
+    itself added with page limit, or the total number of tickets.
+
+    For example: Last ticket shown was 50, total tickets is 100.
+                 The next indices should be 51 to 75
+    For example: Last ticket shown was 100, total tickets is 101.
+                 The next indices should be 101 to 101 (cannot go out of bounds)
+
+    Args:
+        last_ticket_shown (integer): ID of the last ticket displayed
+        number_of_tickets (integer): total number of tickets on the
+                                     Zendesk account
+
+    Returns:
+        start_id (integer): Ticket ID to start from for next page
+        end_id (integer): Ticket ID to end at for next page
+    """
 
     if not isinstance(last_ticket_shown, int) or not isinstance(number_of_tickets, int):
         raise TypeError("last_ticket_shown and number_of_tickets must be integers.")
@@ -72,6 +161,22 @@ def calculateNextPageBounds(last_ticket_shown, number_of_tickets):
 
 
 def calculatePreviousPageBounds(start_id, end_id):
+    """Calculates the range of tickets required to display the previous
+    page of tickets
+
+    Calculating the range of tickets for the previous page depends on the
+    start and end IDs of the current page being displayed. For the previous
+    page, the start index is just decremented by the page limit,
+    but the end index has to be a factor of the page limit.
+
+    Args:
+        start_id (integer): First ticket ID currently displayed
+        end_id (integer): Last ticket ID currently displayed
+
+    Returns:
+        start_id (integer): Ticket ID to start from for prev page
+        end_id (integer): Ticket ID to end at for prev page
+    """
 
     if start_id < modes.TICKETS_PER_PAGE_LIMIT or end_id < start_id:
         raise IndexError("start_id is not in range.")
@@ -91,6 +196,20 @@ def calculatePreviousPageBounds(start_id, end_id):
 
 
 def calculateTotalPages(number_of_tickets):
+    """Calculates the total number of pages required to display
+    all the tickets
+
+    Divides total number of tickets with the page limit. If there
+    still are remaining tickets not accounted for (modulo), add
+    a page
+
+    Args:
+        number_of_tickets (integer): total number of tickets on the
+                                     Zendesk account
+
+    Returns:
+        total_pages (integer): total pages to display all tickets
+    """
 
     if not isinstance(number_of_tickets, int):
         raise TypeError("number_or_tickets should be an integer")
