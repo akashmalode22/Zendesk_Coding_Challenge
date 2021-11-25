12e40ba53403cb0d1d13dab7a7af320d253ec759
Akash Malode <akashmalode22@gmail.com>
2021-11-25 12:26:50 -0500

    Documentation: Docstring comments for modes.py
    This commit:
      - Adds docstring comments for all functions for modes.py
      - Follows the Google Format for docstrings

--- modes.py
+++ modes.py
@@ -18,6 +18,20 @@ class Modes:
     CURRENT_MODE = 0
 
     def validateModeSelection(self, user_input, number_of_tickets=0, from_paging=False):
+        """Validate the user's input according to the mode the program is in.
+
+        The user's input is checked against the current mode's allowed inputs from the
+        input list defined above as OPTIONS_*_*. The user's input is converted to an
+        integer for validating if a selected ticket ID exists.
+
+        Args:
+            user_input (string): the user's input when prompted to enter a menu option
+            number_of_tickets (integer) (optional): number of tickets that exist on the Zendesk account
+            from_paging (boolean) (optional): check whether this function was invoked from paging mode
+
+        Returns:
+            boolean: True if the user's input is valid, False otherwise
+        """
 
         if not isinstance(user_input, str):
             raise TypeError("User input should be of string type.")
@@ -47,6 +61,20 @@ class Modes:
         return False
 
     def validateNextPageExists(self, start_id, end_id, number_of_tickets):
+        """Validate whether a next page exists when paging through tickets.
+
+        The last ticket shown is checked with the total number of tickets. If there are
+        more tickets to be shown after the last ticket, a next page exists.
+
+        Args:
+            start_id (integer): start index of range of tickets that was previously displayed
+            end_id (integer): end index of range of tickets that was previously displayed
+            number_of_tickets (integer): number of tickets that exist on the Zendesk account
+
+        Returns:
+            boolean: True if next page exists, False otherwise.
+        """
+
         if (
             not isinstance(start_id, int)
             or not isinstance(end_id, int)
@@ -60,6 +88,20 @@ class Modes:
         return end_id + 1 <= number_of_tickets
 
     def validatePreviousPageExists(self, start_id, end_id, number_of_tickets):
+        """Validate whether a previous page exists when paging through tickets.
+
+        If the first ticket ID in the range of tickets previously displayed is not yet zero,
+        a previous page exists.
+
+        Args:
+            start_id (integer): start index of range of tickets that was previously displayed
+            end_id (integer): end index of range of tickets that was previously displayed
+            number_of_tickets (integer): number of tickets that exist on the Zendesk account
+
+        Returns:
+            boolean: True if previous page exists, False otherwise.
+        """
+
         if (
             not isinstance(start_id, int)
             or not isinstance(end_id, int)
@@ -73,18 +115,58 @@ class Modes:
         return start_id - 1 > 0
 
     def ticketExists(self, user_input, number_of_tickets):
+        """Checks whether the user's requested ticket ID exists.
+
+        The user's input is checked with the number of tickets that exist on the Zendesk
+        account, and returns a boolean as a result.
+
+        Args:
+            user_input (integer): the user's input when prompted to enter a ticket ID
+            number_of_tickets (integer): number of tickets that exist on the Zendesk account
+
+        Returns:
+            boolean: True if the user's input falls in range of number of existing
+            tickets, False otherwise.
+        """
+
         if not isinstance(user_input, int) or not isinstance(number_of_tickets, int):
             raise TypeError("user_input, number_of_tickets must be integers.")
 
         return user_input > 0 and user_input <= number_of_tickets
 
     def requireMultiplePages(self, number_of_tickets):
+        """Determines whether multiple pages are required to show all tickets
+        (25 tickets per page limit).
+
+        The total number of tickets on the Zendesk account is compared with the
+        tickets per page limit.
+
+        Args:
+            number_of_tickets (integer): number of tickets that exist on the Zendesk account
+
+        Returns:
+            boolean: True if number of tickets exceeds the tickets per page limit,
+            False otherwise.
+        """
+
         if not isinstance(number_of_tickets, int):
             raise TypeError("number_of_tickets must be integers.")
 
         return number_of_tickets > TICKETS_PER_PAGE_LIMIT
 
     def changeMode(self, user_input):
+        """Changes the class variable to the required mode value.
+
+        The class's variable is changed to the user's input mode. Validation was
+        previously done, so it is safe to not validate here.
+
+        Args:
+            user_input (integer): the user's input when prompted to enter a menu option
+
+        Returns:
+            no value
+        """
+
         if not isinstance(user_input, int):
             raise TypeError("mode must be integer.")
 
@@ -94,12 +176,36 @@ class Modes:
         Modes.CURRENT_MODE = user_input
 
     def exit(self, user_input):
+        """Checks whether the user asked to quit the program.
+
+        The user's input is checked with the valid options to quit the program from
+        the OPTIONS_QUIT list.
+
+        Args:
+            user_input (integer): the user's input when prompted to enter a menu option
+
+        Returns:
+            boolean: True if the user's input is a valid quit option, False otherwise.
+        """
+
         if not isinstance(user_input, str):
             raise TypeError("user_input must be a string.")
 
         return user_input in OPTIONS_QUIT
 
     def handleMainMenuMode(self):
+        """Executes main menu mode.
+
+        Main menu options are displayed. The user is asked for a menu option which is
+        validated. If user wishes to quit the program, the program exits. Otherwise,
+        the program's mode is changed to the user's requested mode.
+
+        Args:
+            no value
+
+        Returns:
+            no value
+        """
 
         # Display main menu
         Printer.displayMainMenu()
@@ -120,6 +226,19 @@ class Modes:
         self.changeMode(int(user_input))
 
     def handleAllTicketsMode(self, retriever):
+        """Executes display all tickets mode.
+
+        The program gets the existing number of tickets on the Zendesk account.
+        Program's mode is changed to the appropriate mode if multiple pages are
+        required to display all tickets, or if a single page is sufficient.
+
+        Args:
+            retriever (RetrieveTickets object): a RetrieveTickets class object
+                that stores total number of tickets
+
+        Returns:
+            no value
+        """
 
         # Display all tickets message
         Printer.displayAllTicketsMessage()
@@ -135,6 +254,21 @@ class Modes:
             self.changeMode(MODE_NO_PAGINATION)
 
     def handleSelectedTicketMode(self, retriever, from_paging=False):
+        """Executes display a selected ticket mode.
+
+        User is asked to enter a ticket ID, which is validated if the ticket ID
+        exists. The retriever object requests for the ticket from the API, and the printer
+        class displays the ticket's information. If the method was called from paging mode,
+        the program stays in paging mode. If not, the mode is changed to main menu.
+
+        Args:
+            retriever (RetrieveTickets object): a RetrieveTickets class object
+            from_paging (boolean) (optional): check whether this function was invoked
+                from paging mode
+
+        Returns:
+            no value
+        """
 
         # Get user input for ticket number
         user_input_ticket_number = input("Enter a ticket number: ")
@@ -163,6 +297,17 @@ class Modes:
         self.changeMode(MODE_MAIN_MENU)
 
     def handleSinglePageMode(self, retriever):
+        """Executes display all tickets in a single page.
+
+        API request is made to get all tickets. Details of all tickets are displayed.
+        Mode is changed to main menu mode.
+
+        Args:
+            retriever (RetrieveTickets object): a RetrieveTickets class object
+
+        Returns:
+            no value
+        """
 
         # We have fewer than 25 total tickets. Just display them all
         [tickets, number_of_tickets] = retriever.getAllTicketsNoPagination()
@@ -172,6 +317,19 @@ class Modes:
         self.changeMode(MODE_MAIN_MENU)
 
     def handleMultiplePagesMode(self, retriever):
+        """Executes display all tickets in multiple pages.
+
+        API request is made to get tickets in a range (25 tickets). Details
+        of first 25 tickets are displayed. The program goes into a pagination mode until
+        the user exits the mode. After user exits pagination mode, the range of
+        tickets shown is reset.
+
+        Args:
+            retriever (RetrieveTickets object): a RetrieveTickets class object
+
+        Returns:
+            no value
+        """
 
         # Retrieve the first 25 tickets
         [tickets, number_of_tickets] = retriever.getTicketsInRange(
